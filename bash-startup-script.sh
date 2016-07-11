72a2d5cbdbca8dfdc424b26768a48eb06a42ee07
Ryan Keleti <elitebananart@gmail.com>
2016-07-10 20:02:25 -0400

    Add files via upload

---
+++ bash-startup-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#neofetch --disable title distro icons resolution gpu theme wm font colors --color_blocks pff --gtk_shorthand on --line_wrap off --memory_display infobar --cpu_display off infobar | sed 's/^/     /'
+#cal | sed 's/^/     /'
+
+clear && printf "\e[3J";
+
+echo \
+
+date
+echo \
