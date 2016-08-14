0b07cdf7ba9e0924b9994efbcae62a5e8fc722fa
ryankeleti <elitebananart@gmail.com>
2016-08-14 18:08:25 -0400

    gitupdate.sh >> dotfiles

--- pgrm/euler/p4.c
+++ pgrm/euler/p4.c
@@ -2,7 +2,31 @@
 #include <stdlib.h>
 #include <math.h>
 
+int power(int base, unsigned int exp) {
+  int i, result=1;
+  for(i=0;i<exp;i++) result*=base;
+  return result;
+}
+
 int main() {
+  int n,even,odd;
+
+  system("clear");
+  printf("prints the largest palendromic number from the product of 2 same-digit numbers.\n");
+  printf("input digit size:\t");
+  scanf("%d",&n);
+
+  while (n!=0&&n!='\0') {
+    if (n%2==0) {
+      n = n/2;
+      even = ; 
+    }
+    else {
+      n = (n-1)/2;
+      odd = ;
+    }
+
+  }
 
 
   return 0;
--- pgrm/euler/template.c
+++ pgrm/euler/template.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 
+int power(int base,unsigned int ex) {
+  int i,res=1;
+  for(i=0;i<ex;i++) res*=base;
+  return res;
+}
+
 int main() {
 
 
