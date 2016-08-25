66df087df67dfe7022130d5802fe1c4c18d38974
ryankeleti <elitebananart@gmail.com>
2016-08-25 07:52:35 -0400

    gitupdate.sh >> dotfiles

---
+++ pgrm/knr/p12.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/*  prints F° and C° tables   
+ *  floating-point version
+ */
+
+int main() {
+
+  float fahr,celsius;
+  int lower,upper,step;
+
+  lower = 0;    //  lower limit of table
+  upper = 300;  //  upper limit
+  step = 20;    //  step size
+
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f %6.1f\n",fahr,celsius);
+    fahr = fahr + step;
+  }
+}
+
